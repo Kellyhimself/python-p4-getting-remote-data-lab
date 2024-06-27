898232434be872d57925cf9fa450f3a87f1ab4ab
Kitui Kelly <kituikelly@gmail.com>
2024-06-27 21:02:06 +0300

    compltes lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,20 @@
+# Import the requests and json libraries
 import requests
 import json
 
+# Define a class to make GET requests
 class GetRequester:
-
+    # Initialize the class with a URL
     def __init__(self, url):
         self.url = url
 
+    # Make a GET request and return the response body
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
+    # Load the response body as JSON and return the data
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        json_data = json.loads(response_body)
+        return json_data
